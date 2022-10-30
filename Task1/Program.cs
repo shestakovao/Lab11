6b5431704e3ea5ce3274fe261e769e958a26686b
Алексей Шестаков <shestakov_ao@bk.ru>
2022-10-30 14:11:27 +0700

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения коэфициентов k и b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());    
+
+            Liner liner = new Liner(k, b);
+            Console.WriteLine(liner.Root());
+            Console.ReadKey();  
+
+        }
+    }
+}
---
+++ Task1/Structur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Liner
+    {
+        double k;
+        double b;
+
+        public Liner(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0) 
+            {
+                return $"Деление на ноль. Найти корень не возможно!";
+            } 
+            double x = -b / k;
+            return $"Корень уравнения {x:f3}";
+        }
+    }
+}
